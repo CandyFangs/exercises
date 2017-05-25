33ea2a2b93c99f47f4e8f392d0cbac4620e4dba5
Paulina Osiak <paulina.osiak@gmail.com>
2017-05-25 11:44:59 +0200

    Exercise_1, done

--- exercise_1.rb
+++ exercise_1.rb
@@ -1,1 +1,14 @@
+require 'minitest/autorun'
+
+def sum_odd(n)
+	((0..n).to_a.select{|n| n.odd?}).sum
+end
+
+class TestCalc < MiniTest::Test
+	def test_sum_odd
+		puts sum_odd(6)
+		puts sum_odd(15)
+		puts sum_odd(90)
+	end
+end
 
